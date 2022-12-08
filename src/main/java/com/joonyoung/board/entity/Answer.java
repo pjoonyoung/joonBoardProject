bef9e6591d6f994c2c69e24fb9b7abf2b2b2cd1b
ici906 <ici906@DESKTOP-P4JGH6N>
2022-12-08 16:49:56 +0900

    DB 테이블(entity) 생성완료

--- src/main/java/com/joonyoung/board/entity/Answer.java
+++ src/main/java/com/joonyoung/board/entity/Answer.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -14,18 +15,20 @@ import lombok.NoArgsConstructor;
 
 @Entity
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 public class Answer {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
 	private Integer id;//답변게시판 번호
 	
 	@Column(length = 1000)
 	private String content;//답변게시판 내용
 	
-	private LocalDateTime createTime;//답변게시판 등록일시
+	private LocalDateTime createTime;
 	
-	private Question question;//질문게시판 객체(질문게시판의 id값 추출)
+	@ManyToOne//n:1 구조(질문1개에 답변 여러개가 달리는 구조-부모(질문)자식(답변)관계)
+	private Question question;//질문게시판 객체(질문게시판의 id(외래키)를 가져오는 필드가 생성됨)
+	
 }
--- src/main/java/com/joonyoung/board/entity/Question.java
+++ src/main/java/com/joonyoung/board/entity/Question.java
@@ -1,12 +1,15 @@
 package com.joonyoung.board.entity;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -14,19 +17,23 @@ import lombok.NoArgsConstructor;
 
 @Entity
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 public class Question {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;//게시판 번호
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private Integer id;//질문게시판 번호
 	
 	@Column(length = 100)
 	private String subject;//질문게시판 제목
 	
 	@Column(length = 1000)
-	private String content;//게시판 내용
+	private String content;//질문게시판 내용
 	
 	private LocalDateTime createDate;//글 등록일시
+	
+	@OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+	private List<Answer> answerList;//1:n 구조
+	
 }
